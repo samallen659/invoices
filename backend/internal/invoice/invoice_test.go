1fcdb91a5b8d42b9b352e2a0efc5a2f0f4139039
Sam Allen <samallen659@gmail.com>
2023-11-11 15:02:36 +0000

    adds tests to CreatedAt and Total for NewInvoice

--- backend/internal/invoice/invoice_test.go
+++ backend/internal/invoice/invoice_test.go
@@ -6,10 +6,19 @@ import (
 )
 
 func TestInvoice(t *testing.T) {
-	t.Run("NewInvoice returns an Invoice with UUID set ID", func(t *testing.T) {
+	t.Run("NewInvoice returns an Invoice with UUID set ID, CreatedAt set and 0 Money for total", func(t *testing.T) {
 		in := invoice.NewInvoice()
+
 		if in.ID.String() == "00000000-0000-0000-0000-000000000000" {
 			t.Errorf("ID on invoice not set to valid UUID: %s", in.ID.String())
+		}
+
+		if in.CreatedAt.String() == "0001-01-01 00:00:00 +0000 UTC" {
+			t.Errorf("CreatedAt on invoice not set to valid time: %s", in.CreatedAt.String())
+		}
+
+		if in.Total.Currency().Code != "GBP" {
+			t.Errorf("Total not initialised as GBP")
 		}
 	})
 }
