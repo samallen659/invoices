a547ba336542a7e26365d7b38569f34a3bdb27f0
Sam Allen <samallen659@gmail.com>
2023-11-15 20:10:04 +0000

    adds InvoiceItemFactory base

--- backend/internal/invoice/item.go
+++ backend/internal/invoice/item.go
@@ -1,5 +1,7 @@
 package invoice
 
+import "errors"
+
 type Item struct {
 	Name  string
 	Price float32
@@ -9,4 +11,19 @@ type InvoiceItem struct {
 	Quantity int
 	Item     Item
 	Total    float32
+}
+
+func InvoiceItemFactory(name string, price float32, quantity int) (*InvoiceItem, error) {
+	if name == "" {
+		return nil, errors.New("name cannot be emtpy")
+	}
+	if price <= 0.0 {
+		return nil, errors.New("price must be greater than 0")
+	}
+	if quantity <= 0 {
+		return nil, errors.New("quantity must be greater than 0")
+	}
+
+	//TODO
+	return nil, nil
 }
