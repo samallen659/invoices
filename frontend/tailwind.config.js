c7a8968d67af813905558b5de1eb04191bb48b70
Sam Allen <samallen659@gmail.com>
2023-11-23 19:33:19 +0000

    adds custom tailwind design system

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,9 +4,34 @@ export default {
         "./index.html",
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
+    darkMode: 'class',
     theme: {
         extend: {},
+        fontFamily: {
+            sans: ["League Spartan", "sans-serif"],
+        },
+        colors: {
+            purple: {
+                400: "#7C5DFA",
+                600: "#9277FF",
+            },
+            gray: {
+                200: "#DFE3FA",
+                400: "#888EB0",
+                600: "#252945",
+                800: "#1E2139",
+            },
+            indigo: {
+                400: "7E88C3",
+                800: "#141625",
+            },
+            red: {
+                200: "#9277FF",
+                400: "#EC5757",
+            },
+            "off-white": "#F8F8FB",
+            white: "#FFFFFF",
+        }
     },
     plugins: [],
 }
-
