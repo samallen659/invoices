353d44fe34438ffeec575500e2d4aff24150cd00
Sam Allen <samallen659@gmail.com>
2023-11-23 19:40:16 +0000

    updates custom tailwind design system

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
     return (
         <>
-            <div className="bg-red-500">
-                <p className="text-lg text-sky-50">hello world!</p>
+            <div className="bg-gray-800">
+                <p className="text-lg text-red-400">hello world!</p>
             </div>
         </>
     )
