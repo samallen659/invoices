660d892184d2ee8ccef359747a87f55322ed0cac
Sam Allen <samallen659@gmail.com>
2023-11-28 19:50:59 +0000

    updates app layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,26 @@
-import { useState } from "react"
-import { SideBar } from "./components/SideBar/SideBar"
+import { useState } from "react";
+import { SideBar } from "./components/SideBar/SideBar";
+import { MenuBar } from "./components/MenuBar/MenuBar";
 
 function App() {
-    const [isDark, setIsDark] = useState(false)
+	const [isDark, setIsDark] = useState(false);
 
-    const toggleIsDark = () => {
-        setIsDark((prev) => !prev)
-    }
+	const toggleIsDark = () => {
+		setIsDark((prev) => !prev);
+	};
 
-    return (
-        <>
-            <div className={`bg-gray-800 ${isDark ? "dark" : "light"}`}>
-                <main className="bg-off-white dark:bg-indigo-600 h-screen">
-                    <SideBar toggleIsDark={toggleIsDark} isDark={isDark} />
-                </main>
-            </div >
-        </>
-    )
+	return (
+		<>
+			<div className={`bg-gray-800 ${isDark ? "dark" : "light"}`}>
+				<main className="flex h-screen flex-col bg-off-white dark:bg-indigo-600 lg:flex-row">
+					<SideBar toggleIsDark={toggleIsDark} isDark={isDark} />
+					<section className="mx-auto mt-16 w-full max-w-[327px]  md:max-w-[672px] lg:max-w-2xl xl:max-w-4xl 2xl:max-w-6xl">
+						<MenuBar />
+					</section>
+				</main>
+			</div>
+		</>
+	);
 }
 
-export { App }
+export { App };
