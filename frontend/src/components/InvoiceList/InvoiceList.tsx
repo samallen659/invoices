96feefe77c983a4feeb45c4c0834aa8ba310bbc2
Sam Allen <samallen659@gmail.com>
2023-12-16 14:46:13 +0000

    moves arrow right icon to icons file

--- frontend/src/components/InvoiceList/InvoiceList.tsx
+++ frontend/src/components/InvoiceList/InvoiceList.tsx
@@ -2,6 +2,7 @@ import { Invoice } from "../../types";
 import { InvoiceIdx } from "../../App";
 import { InvoiceStatusBox } from "../InvoiceStatusBox/InvoiceStatusBox";
 import { getShortID, getShortDate } from "../../utils";
+import { IconArrowRight } from "../Icons";
 
 type InvoiceListProps = {
 	invoices: Invoice[];
@@ -49,14 +50,6 @@ function InvoiceListItem({ idx, invoice, handleInvoiceIdxChange }: InvoiceListIt
 				</div>
 			</div>
 		</li>
-	);
-}
-
-function IconArrowRight() {
-	return (
-		<svg width="7" height="10" xmlns="http://www.w3.org/2000/svg">
-			<path d="M1 1l4 4-4 4" stroke="#7C5DFA" strokeWidth="2" fill="none" fillRule="evenodd" />
-		</svg>
 	);
 }
 
