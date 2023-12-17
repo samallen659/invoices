e87297d5e6affe7fb9653ef6a7d28a4db00e25ca
Sam Allen <samallen659@gmail.com>
2023-12-17 15:10:23 +0000

    Removes IconLeftArrow function and imports from Icons file

--- frontend/src/components/ViewInvoice/ViewInvoice.tsx
+++ frontend/src/components/ViewInvoice/ViewInvoice.tsx
@@ -1,6 +1,7 @@
 import { Invoice, Item } from "../../types";
 import { getShortDate, getShortID } from "../../utils";
 import { InvoiceStatusBox } from "../InvoiceStatusBox/InvoiceStatusBox";
+import { IconLeftArrow } from "../Icons";
 
 type ViewInvoiceProps = {
 	invoice: Invoice;
@@ -132,17 +133,4 @@ function ViewInvoiceDetails({ invoice }: ViewInvoiceDetailsProps) {
 	);
 }
 
-function IconArrowLeft() {
-	return (
-		<svg width="7" height="10" xmlns="http://www.w3.org/2000/svg">
-			<path
-				d="M6.342.886L2.114 5.114l4.228 4.228"
-				stroke="#9277FF"
-				strokeWidth="2"
-				fill="none"
-				fillRule="evenodd"
-			/>
-		</svg>
-	);
-}
 export { ViewInvoice };
