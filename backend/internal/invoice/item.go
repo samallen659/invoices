b597599899ca02fc37a37a41e7a5cb562f433b24
Sam Allen <samallen659@gmail.com>
2023-12-23 13:04:07 +0000

    Add Money struct and implements sql/driver Valuer and sql Scanner

--- backend/internal/invoice/item.go
+++ backend/internal/invoice/item.go
@@ -1,6 +1,12 @@
 package invoice
 
-import "errors"
+import (
+	"database/sql/driver"
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 type Item struct {
 	Name  string
@@ -12,6 +18,8 @@ type InvoiceItem struct {
 	Item     Item
 	Total    float64
 }
+
+var MONEY_SCAN_ERROR = errors.New("Failed to scan into Money")
 
 func NewInvoiceItem(name string, price float64, quantity int) (*InvoiceItem, error) {
 	if name == "" {
@@ -34,4 +42,45 @@ func NewInvoiceItem(name string, price float64, quantity int) (*InvoiceItem, err
 		Total:    total,
 	}
 	return invoiceItem, nil
+}
+
+type Money struct {
+	pounds int
+	pence  int
+}
+
+// Implements sql/driver Valuer interface
+func (m *Money) Value() (driver.Value, error) {
+	return fmt.Sprintf("%d.%d", m.pounds, m.pence), nil
+}
+
+// Implements sql Scanner interface
+func (m *Money) Scan(value any) error {
+	if value == nil {
+		m.pounds = 0
+		m.pence = 0
+		return nil
+	}
+
+	if sv, err := driver.String.ConvertValue(value); err == nil {
+		if v, ok := sv.(string); ok {
+			data := strings.Split(v, ".")
+			if len(data) < 2 || len(data) > 2 {
+
+				return MONEY_SCAN_ERROR
+			}
+			pounds, err := strconv.Atoi(data[0])
+			if err != nil {
+				return MONEY_SCAN_ERROR
+			}
+			pence, err := strconv.Atoi(data[1])
+			if err != nil {
+				return MONEY_SCAN_ERROR
+			}
+			m.pounds = pounds
+			m.pence = pence
+			return nil
+		}
+	}
+	return MONEY_SCAN_ERROR
 }
