f5caa5858c074eb9204a07e33247a273a1ca36ef
Sam Allen <samallen659@gmail.com>
2023-12-30 15:16:38 +0000

    adds functionality to repo

--- backend/internal/user/repository.go
+++ backend/internal/user/repository.go
@@ -1,6 +1,9 @@
 package user
 
 import (
+	"database/sql"
+	"errors"
+
 	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
@@ -23,17 +26,41 @@ func NewPostgresRepository(conn *sqlx.DB) *PostgresRepository {
 }
 
 func (p *PostgresRepository) GetUser(ctx context.Context, id uuid.UUID) (*User, error) {
-	return nil, nil
+	var user User
+	err := p.conn.QueryRowx(`SELECT id, first_name, last_name, email FROM users WHERE id=$1`,
+		id).Scan(&user.ID, &user.FirstName, &user.LastName, &user.Email)
+	if err == sql.ErrNoRows {
+		return nil, errors.New("No User found with supplied id")
+	}
+	if err != nil {
+		return nil, errors.New("failed to run query against database")
+	}
+
+	return &user, nil
 }
 
 func (p *PostgresRepository) StoreUser(ctx context.Context, user User) error {
+	err := p.conn.QueryRowx(`INSERT INTO users(id, first_name, last_name, email) VALUES ($1, $2, $3, $4)`,
+		user.ID, user.FirstName, user.LastName, user.Email)
+	if err != nil {
+		return errors.New("failed to insert into users")
+	}
 	return nil
 }
 
 func (p *PostgresRepository) DeleteUser(ctx context.Context, id uuid.UUID) error {
+	err := p.conn.QueryRowx(`DELETE FROM users WHERE id=$1`, id)
+	if err != nil {
+		return errors.New("failed to delete from users")
+	}
 	return nil
 }
 
 func (p *PostgresRepository) UpdateUser(ctx context.Context, user User) error {
+	err := p.conn.QueryRowx(`UPDATE users SET first_name=$2, last_name=$3, email=$4 WHERE id=$1`,
+		user.ID, user.FirstName, user.LastName, user.Email)
+	if err != nil {
+		return errors.New("failed to update users")
+	}
 	return nil
 }
