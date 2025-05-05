52cd5f262719414db11d69b6cd16a813879472e3
Sam Allen <samallen659@gmail.com>
2025-05-05 14:49:41 +0100

    adds jwtMiddleware and generate functions

---
+++ backend/internal/user/jwt.go
@@ -0,0 +1,46 @@
+package user
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var jwtKey = []byte(fmt.Sprintf(os.Getenv("JWT_KEY")))
+
+func generateJWT(userName string) (string, error) {
+	claims := jwt.MapClaims{
+		"user_name": userName,
+		"exp":       time.Now().Add(time.Hour * 24).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtKey)
+}
+
+func jwtMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		tokenStr := r.Header.Get("Authorization")
+		if tokenStr == "" || !strings.HasPrefix(tokenStr, "Bearer ") {
+			http.Error(w, "Missing token", http.StatusUnauthorized)
+			return
+		}
+
+		tokenStr = strings.TrimPrefix(tokenStr, "Bearer ")
+
+		token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (any, error) {
+			return jwtKey, nil
+		})
+
+		if err != nil || !token.Valid {
+			http.Error(w, "Invalid token", http.StatusUnauthorized)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
