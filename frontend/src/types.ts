1479cbeb5bf8242081df3bde314436054e86fdd4
Sam Allen <samallen659@gmail.com>
2025-05-06 19:09:33 +0100

    adds User, LoginRequest, SignUpRequest and GetUserRequest types

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,62 +1,101 @@
 type InvoiceStatus = "pending" | "paid" | "draft";
 
 type Address = {
-	Street: string;
-	City: string;
-	PostCode: string;
-	Country: string;
+  Street: string;
+  City: string;
+  PostCode: string;
+  Country: string;
 };
 
 type Item = {
-	Name: string;
-	Quantity: number;
-	Price: number;
-	Total: number;
+  Name: string;
+  Quantity: number;
+  Price: number;
+  Total: number;
 };
 
 type ItemRes = {
-	Name: string;
-	Price: number;
+  Name: string;
+  Price: number;
 };
 
 type InvoiceItem = {
-	Item: ItemRes;
-	Quantity: number;
-	Total: number;
+  Item: ItemRes;
+  Quantity: number;
+  Total: number;
 };
 
 type Client = {
-	ClientName: string;
-	ClientEmail: string;
+  ClientName: string;
+  ClientEmail: string;
 };
 
 type Invoice = {
-	ID: string;
-	CreatedAt: string;
-	PaymentDue: string;
-	Description: string;
-	PaymentTerms: number;
-	ClientName: string;
-	ClientEmail: string;
-	Status: InvoiceStatus;
-	ClientAddress: Address;
-	SenderAddress: Address;
-	Items: Item[];
-	Total: number;
+  ID: string;
+  CreatedAt: string;
+  PaymentDue: string;
+  Description: string;
+  PaymentTerms: number;
+  ClientName: string;
+  ClientEmail: string;
+  Status: InvoiceStatus;
+  ClientAddress: Address;
+  SenderAddress: Address;
+  Items: Item[];
+  Total: number;
 };
 
 type InvoiceRes = {
-	ID: string;
-	CreatedAt: Date;
-	PaymentDue: Date;
-	Description: string;
-	PaymentTerms: number;
-	Client: Client;
-	Status: InvoiceStatus;
-	ClientAddress: Address;
-	SenderAddress: Address;
-	InvoiceItems: InvoiceItem[];
-	Total: number;
+  ID: string;
+  CreatedAt: Date;
+  PaymentDue: Date;
+  Description: string;
+  PaymentTerms: number;
+  Client: Client;
+  Status: InvoiceStatus;
+  ClientAddress: Address;
+  SenderAddress: Address;
+  InvoiceItems: InvoiceItem[];
+  Total: number;
 };
 
-export type { InvoiceStatus, Address, Item, InvoiceItem, Client, Invoice, InvoiceRes, ItemRes };
+type User = {
+  UserName: string;
+  FirstName: string;
+  LastName: string;
+  Email: string;
+};
+
+type LoginRequest = {
+  UserName: string;
+  Email: string;
+  Password: string;
+};
+
+type SignUpRequest = {
+  UserName: string;
+  FirstName: string;
+  LastName: string;
+  Email: string;
+  Password: string;
+};
+
+type GetUserRequest = {
+  UserName: string;
+  Email: string;
+};
+
+export type {
+  InvoiceStatus,
+  Address,
+  Item,
+  InvoiceItem,
+  Client,
+  Invoice,
+  InvoiceRes,
+  ItemRes,
+  User,
+  LoginRequest,
+  SignUpRequest,
+  GetUserRequest,
+};
