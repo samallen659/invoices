73d59da31faec348c2a53106169416d95764543e
Sam Allen <samallen659@gmail.com>
2025-05-06 19:19:06 +0100

    adds Signup handle func

--- backend/internal/user/handler.go
+++ backend/internal/user/handler.go
@@ -60,6 +60,30 @@ func (h *Handler) HandleLogin(w http.ResponseWriter, r *http.Request) {
 	utils.WriteJson(w, 200, `{"token":"`+jwt+`"}`)
 }
 
+func (h *Handler) HandleSignup(w http.ResponseWriter, r *http.Request) {
+	var signupReq SignUpRequest
+
+	err := json.NewDecoder(r.Body).Decode(&signupReq)
+	if err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+	}
+
+	success, err := h.svc.SignUpUser(r.Context(), signupReq)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+	if !success {
+		http.Error(w, "Failed to sign up user", http.StatusInternalServerError)
+	}
+
+	jwt, err := generateJWT(signupReq.UserName)
+	if err != nil {
+		http.Error(w, "Failed to generate JWT", http.StatusInternalServerError)
+	}
+
+	utils.WriteJson(w, 200, `{"token":"`+jwt+`"}`)
+}
+
 func (h *Handler) HandleGetUser(w http.ResponseWriter, r *http.Request) {
 	var getUserReq GetUserRequest
 	err := json.NewDecoder(r.Body).Decode(&getUserReq)
